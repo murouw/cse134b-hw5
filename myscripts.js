404e35a2f3ebe047273dc12a226d01e9a1fd72bc
Murou Wang <muwang@ucsd.edu>
2025-03-15 00:19:13 -0700

    Fix portfolio nav
    Update script so theme changer works on portfolio page and fix styling so button is consistent with main page

--- myscripts.js
+++ myscripts.js
@@ -16,21 +16,21 @@ function calculateSettingAsThemeString({
 // FIX W ICON WOOO
 function updateButton({ buttonEl, isDark, image }) {
   const newCta = isDark ? "Change to light theme" : "Change to dark theme";
-  const newImg = isDark ?  "images/moon.png" : "images/sun.png";
+  const newImg = isDark ? "images/moon.png" : "images/sun.png";
 
   buttonEl.setAttribute("aria-label", newCta);
   image.setAttribute("src", newImg);
-  console.log(newImg)
 }
 
 function updateThemeOnEl({ theme }) {
   document.querySelector("html").setAttribute("data-theme", theme);
-  document.getElementById("contact").setAttribute("data-theme", theme);
+  if (document.getElementById("contact"))
+    document.getElementById("contact").setAttribute("data-theme", theme);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
   const button = document.querySelector("[data-theme-toggle]");
-  const themeImage = button.querySelector('img');
+  const themeImage = button.querySelector("img");
 
   const localStorageTheme = localStorage.getItem("theme");
   const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
@@ -40,24 +40,32 @@ document.addEventListener("DOMContentLoaded", function () {
     systemSettingDark,
   });
 
-  updateButton({ buttonEl: button, isDark: currentThemeSetting === "dark", image: themeImage});
+  updateButton({
+    buttonEl: button,
+    isDark: currentThemeSetting === "dark",
+    image: themeImage,
+  });
   updateThemeOnEl({ theme: currentThemeSetting });
 
   button.addEventListener("click", (event) => {
     const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
 
     localStorage.setItem("theme", newTheme);
-    updateButton({ buttonEl: button, isDark: newTheme === "dark", image: themeImage});
+    updateButton({
+      buttonEl: button,
+      isDark: newTheme === "dark",
+      image: themeImage,
+    });
     updateThemeOnEl({ theme: newTheme });
 
     currentThemeSetting = newTheme;
 
-    if (newTheme === 'dark') {
-        themeIcon.classList.remove('fa-sun');
-        themeIcon.classList.add('fa-moon');
-      } else {
-        themeIcon.classList.remove('fa-moon');
-        themeIcon.classList.add('fa-sun');
-      }
+    if (newTheme === "dark") {
+      themeIcon.classList.remove("fa-sun");
+      themeIcon.classList.add("fa-moon");
+    } else {
+      themeIcon.classList.remove("fa-moon");
+      themeIcon.classList.add("fa-sun");
+    }
   });
 });
